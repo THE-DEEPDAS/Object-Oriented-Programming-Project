e0f6320dae71a178284801aa728983c619994088
THE-DEEPDAS <deepdblm@gmail.com>
2024-10-01 16:57:23 +0530

    basic setup

---
+++ root.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long;
+
+class AES{
+    public:
+    // aes
+};
+
+class RSA{
+    public:
+    // rsa
+};
+
+class ECC{
+    public:
+    // ecc
+};
+
+int main(){
+    cout << "Welcome to our encrytion - decryption program." << endl;
+    return 0;
+}
